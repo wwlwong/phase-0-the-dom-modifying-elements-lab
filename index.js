1ba4a09f0fd91cb33e4658b5b169c7d1ef2bd7db
William Wong <william.wai.lim.wong@gmail.com>
2023-04-04 13:43:53 -0400

    Completed dome modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+ main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+newHeader.textContent = "William is the champion";
+document.body.append(newHeader);
+
